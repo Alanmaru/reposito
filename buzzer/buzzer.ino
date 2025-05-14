a0393c6b93de5fe44df156dec1201484fb8a3a7d
AlanMaru <maruchoalan4@gmail.com>
2025-05-14 20:56:16 -0300

    ns

---
+++ buzzer/buzzer.ino
@@ -0,0 +1,72 @@
+#include <Me4Button.h>
+#include <Me7SegmentDisplay.h>
+#include <MeAuriga.h>
+#include <MeBaseBoard.h>
+#include <MeBluetooth.h>
+#include <MeBuzzer.h>
+#include <MeColorSensor.h>
+#include <MeCompass.h>
+#include <MeConfig.h>
+#include <MeDCMotor.h>
+#include <MeEEPROM.h>
+#include <MeEncoderMotor.h>
+#include <MeEncoderNew.h>
+#include <MeEncoderOnBoard.h>
+#include <MeFlameSensor.h>
+#include <MeGasSensor.h>
+#include <MeGyro.h>
+#include <MeHostParser.h>
+#include <MeHumitureSensor.h>
+#include <MeIR.h>
+#include <MeInfraredReceiver.h>
+#include <MeJoystick.h>
+#include <MeLEDMatrix.h>
+#include <MeLEDMatrixData.h>
+#include <MeLightSensor.h>
+#include <MeLimitSwitch.h>
+#include <MeLineFollower.h>
+#include <MeMCore.h>
+#include <MeMbotDCMotor.h>
+#include <MeMegaPi.h>
+#include <MeMegaPiDCMotor.h>
+#include <MeMegaPiPro.h>
+#include <MeMegaPiPro4DcMotor.h>
+#include <MeMegaPiProESCMotor.h>
+#include <MeOnBoardTemp.h>
+#include <MeOneWire.h>
+#include <MeOrion.h>
+#include <MePIRMotionSensor.h>
+#include <MePS2.h>
+#include <MePm25Sensor.h>
+#include <MePort.h>
+#include <MePotentiometer.h>
+#include <MePressureSensor.h>
+#include <MeRGBLed.h>
+#include <MeSerial.h>
+#include <MeShield.h>
+#include <MeShutter.h>
+#include <MeSmartServo.h>
+#include <MeSoundSensor.h>
+#include <MeStepper.h>
+#include <MeStepperOnBoard.h>
+#include <MeSuperVariable.h>
+#include <MeTemperature.h>
+#include <MeTouchSensor.h>
+#include <MeUSBHost.h>
+#include <MeUltrasonicSensor.h>
+#include <MeVoice.h>
+#include <MeWifi.h>
+
+
+
+#define b 45
+#define do 
+
+void setup() {
+ pinMode(b, OUTPUT);
+ 
+}
+
+void loop() {
+ analogWrite(b;45);
+}
